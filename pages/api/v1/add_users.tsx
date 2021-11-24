6f4affa70c4b8026679ef48af25b923eeb6461dd
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-24 22:07:27 +0800

    将校验逻辑放到 user.ts

--- pages/api/v1/add_users.tsx
+++ pages/api/v1/add_users.tsx
@@ -7,42 +7,17 @@ import { User } from "src/entity/User";
 const Posts: NextApiHandler = async (req, res) => {
     // 获取用户上传的参数 通过req.body
     const { username, password, passwordConfirmation } = req.body;
-    let errors = {
-        username: [],
-        password: [],
-        passwordConfirmation: [],
-    }
-    // 用户名错误不匹配
-    if (username.trim() === '') {
-        errors.username.push("用户名为空")
-    }
-    if (!/[a-zA-Z0-9]/.test(username.trim())) {
-        errors.username.push("格式不合法")
-    }
-    if (username.trim().length > 8) {
-        errors.username.push("长度不得超出8位")
-    }
-    if (username.trim().length <= 3) {
-        errors.username.push("长度不得小于3位")
-    }
-
-    // 密码不匹配的错误处理
-    if (password.trim().length === "") {
-        errors.password.push("请输入密码")
-    }
-    if (password !== passwordConfirmation) {
-        errors.passwordConfirmation.push("密码不匹配")
-    }
-    const fount = Object.values(errors).find(item => item.length > 0)
+    const connection = await getDatabaseConnection()
     res.setHeader('Content-Type', 'application/json;charset=utf-8')
-    if (fount) {
+    const user = new User()
+    user.username = username.trim()
+    user.password = password
+    user.passwordConfirmation = passwordConfirmation
+    await user.validate()
+    if (await user.hasError()) {
         res.statusCode = 422
-        res.write(JSON.stringify(errors))
+        res.write(JSON.stringify(user.errors))
     } else {
-        const connection = await getDatabaseConnection()
-        const user = new User()
-        user.username = username.trim()
-        user.passwordDigest = md5(password)
         // 将数据保存下来
         await connection.manager.save(user)
         // 保存到数据库之后将数据返回给前端
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Post } from "./Post";
 import { Comment } from "./Comment";
+import { getDatabaseConnection } from "lib/getDatabaseConnection";
 
 @Entity("users")
 export class User {
@@ -11,4 +12,45 @@ export class User {
     @UpdateDateColumn() updatedAt: Date;
     @OneToMany(type => Post, post => post.author) posts: Post[];
     @OneToMany(type => Comment, comment => comment.user) comments: Comment[];
+
+    errors = {
+        username: [] as string[],
+        password: [] as string[],
+        passwordConfirmation: [] as string[],
+    }
+
+    password: string
+    passwordConfirmation: string
+    async validate() {
+        // 用户名错误不匹配
+        if (this.username.trim() === '') {
+            this.errors.username.push("用户名为空")
+        }
+        if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+            this.errors.username.push("格式不合法")
+        }
+        if (this.username.trim().length > 8) {
+            this.errors.username.push("长度不得超出8位")
+        }
+        if (this.username.trim().length <= 3) {
+            this.errors.username.push("长度不得小于3位")
+        }
+        // 先看数据库中是否已经存在当前username
+        const found = await (await getDatabaseConnection()).manager.find(User, { username: this.username })
+        if (found.length > 0) {
+            this.errors.username.push("用户名已存在")
+        }
+
+        // 密码不匹配的错误处理
+        if (!this.password.trim()) {
+            this.errors.password.push("请输入密码")
+        }
+        if (this.password !== this.passwordConfirmation) {
+            this.errors.passwordConfirmation.push("密码不匹配")
+        }
+    }
+
+    hasError() {
+        return !!Object.values(this.errors).find(item => item.length > 0)
+    }
 }
