57d93bc7c52f6da83d5324ed5524319a56c7c94e
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-28 11:09:46 +0800

    .env.local文件

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -1,10 +1,14 @@
 import { NextApiHandler } from "next";
 import { withIronSession } from "next-iron-session"
 
+console.log('====================================');
+console.log(process.env.SECRET);
+console.log('====================================');
+
 export default function WithSession(handler: NextApiHandler) {
     return withIronSession(handler, {
         // 密码还得优化
-        password: "bd569350-9d78-4ee1-bc62-4f4079093069",
+        password: process.env.SECRET,
         cookieName: "blog",
         // 控制https，取消
         cookieOptions: { secure: false },
--- next-env.d.ts
+++ next-env.d.ts
@@ -4,12 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-import * as next from "next";
-import { Session } from "next-iron-session";
-
-declare module "next" {
-    interface NextApiRequest {
-        session: Session;
-    }
-}
