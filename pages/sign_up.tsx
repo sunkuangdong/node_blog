afba1c2e7e1a4b6369b8442411ca148d2b025458
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-28 21:57:16 +0800

    sign_up 更改

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -1,81 +1,42 @@
 import axios from "axios";
 import Form from "components/Form";
+import useForm from "hooks/useForm";
 import { NextPage } from "next";
 import { useCallback, useState } from "react";
 
 // 注册页面
 const SignUp: NextPage = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: '',
-        passwordConfirmation: ''
-    })
-    const [errors, setErrors] = useState({
-        username: [],
-        password: [],
-        passwordConfirmation: []
-    })
-    // 提交表单事件
-    // 使用 useCallback 会有一个缓存的优化
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
+    const initFormData = { username: '', password: '', passwordConfirmation: '' }
+    const buttons = <div> <button>提交</button> </div>
+    const onSubmit = (formData: typeof initFormData) => {
         axios.post('/api/v1/add_users', formData)
-            .then(res => {
+            .then(() => {
                 window.alert("注册成功")
                 window.location.href = '/sign_in';
-                setErrors({
-                    username: [],
-                    password: [],
-                    passwordConfirmation: []
-                })
             }, (err) => {
                 if (err.response && err.response.status === 422) {
                     setErrors({
-                        ...errors,
+                        ...err,
                         ...err.response.data
                     })
                 }
             })
-    }, [formData])
+    }
+    const { form, setErrors } = useForm({
+        initFormData,
+        fields: [
+            { label: "用户名", type: "text", key: "username" },
+            { label: "密码", type: "password", key: "password" },
+            { label: "确认密码", type: "password", key: "passwordConfirmation" },
+        ],
+        buttons, onSubmit
+    })
 
-    const onChange = useCallback((key, value) => {
-        setFormData({
-            ...formData,
-            [key]: value,
-        })
-    }, [formData])
     return (
-        <>
+        <div>
             <h1>注册</h1>
-            <Form fields={[
-                {
-                    label: "用户名",
-                    type: "text",
-                    value: formData.username,
-                    onChange: e => onChange("username", e.target.value),
-                    errors: errors.username
-                },
-                {
-                    label: "密码",
-                    type: "password",
-                    value: formData.password,
-                    onChange: e => onChange("password", e.target.value),
-                    errors: errors.password
-                },
-                {
-                    label: "确认密码",
-                    type: "password",
-                    value: formData.passwordConfirmation,
-                    onChange: e => onChange("passwordConfirmation", e.target.value),
-                    errors: errors.passwordConfirmation
-                },
-            ]} onSubmit={onSubmit} buttons={
-                <div>
-                    <button>注册</button>
-                </div>
-            }>
-            </Form>
-        </>
+            {form}
+        </div>
     )
 }
 
